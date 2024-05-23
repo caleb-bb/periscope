f9f42ee3286719cb3302b169c396775d3280ca09
Caleb <caleb@MacBook-Pro.local>
2024-05-23 13:04:29 -0400

    Remove superfluous functions and add message/1

--- lib/periscope.ex
+++ lib/periscope.ex
@@ -25,10 +25,10 @@ defmodule Periscope do
     |> Enum.map(&elem(&1, 0))
   end
 
-  # Helper function for extracting state from a list of pids.
-  defp component_states do
-    Enum.map(liveview_pids(), &:sys.get_state/1)
-  end
+  @doc ~S"""
+  Used for testing handle_info/2 callback.
+  """
+  def message(msg, socket_number // 0), do: send(socket(socket_number).assigns.root_pid, msg)
 
   @doc ~S"""
   Returns the sockets for all active liveviews in a 0-indexed map. So all_sockets(0) will return the first socket in the map.
@@ -82,49 +82,6 @@ defmodule Periscope do
   end
 
   @doc ~S"""
-  Takes the last part of a schema module name and returns all the fields in that schema. So running `schema_fields("Comments")` in an app called MyBlog will return all fields for MyBlog.Schemas.Comments..
-  """
-  @spec schema_fields(binary) :: list
-  def schema_fields(schema_module) do
-    schema_name = Module.concat(application_name(), "Schemas." <> schema_module)
-
-    schema_name.__schema__(:fields)
-  end
-
-  @doc ~S"""
-  Searches through a nested map (such as socket.assigns) and checks if `key` exists at any point in the tree. Useful for finding out if a given key is somewhere in your assigns without scrolling through pages of socket info.
-  """
-  @spec deep_has_key?(map, atom) :: boolean
-  def deep_has_key?(map, key), do: Enum.member?(all_keys(map), key)
-
-  @doc ~S"""
-  Will return a map that takes liveviews to lists of paths to those views. Useful if you want to access a component fast without having to scroll through the router and figure out its URL.
-  """
-  @spec liveviews_to_paths :: list
-  def liveviews_to_paths do
-    app = application_name() <> "Web"
-
-    Module.concat([app, "Router"])
-    |> liveviews_to_paths()
-  end
-
-  defp liveviews_to_paths(your_app_web) do
-    your_app_web.__routes__()
-    |> Enum.filter(&is_a_liveview_route?/1)
-    |> Enum.map(&liveview_route_to_path/1)
-    |> Enum.reduce(%{}, &aggregate_merge(&1, &2))
-  end
-
-  defp liveview_route_to_path(route) do
-    {liveview, path} = {
-      route.metadata.phoenix_live_view |> elem(0),
-      route.path
-    }
-
-    %{liveview => [path]}
-  end
-
-  @doc ~S"""
   Returns a map whose keys are component names (as those found in component_names) and whose values are the assigns for those components.
   """
   @spec components_to_assigns :: map
@@ -146,40 +103,8 @@ defmodule Periscope do
     Map.get(components_to_assigns(), component)
   end
 
-  @doc ~S"""
-  Takes a route and returns true if it's a route to a liveview module.
-  """
-  @spec is_a_liveview_route?(atom | map) :: boolean
-  def is_a_liveview_route?(route) do
-    Map.has_key?(route.metadata, :phoenix_live_view)
+  # Helper function for extracting state from a list of pids.
+  defp component_states do
+    Enum.map(liveview_pids(), &:sys.get_state/1)
   end
-
-  defp all_keys(some_map) do
-    keys = Map.keys(some_map)
-    next_layer = keys
-    |> Enum.filter(fn key -> is_map(Map.get(some_map, key)) end)
-
-    Enum.flat_map(next_layer, &all_keys(Map.get(some_map, &1))) ++ keys
-
-  end
-
-  defp aggregate_merge(a, b) do
-    Map.merge(a, b, fn _k, v1, v2 -> List.flatten([v1, v2]) end)
-  end
-
-  @spec application_name :: String.t()
-  defp application_name do
-    {:ok, lib_dir} =
-      (Path.expand("") <> "/lib")
-      |> File.ls()
-
-    lib_dir
-    |> Enum.filter(&String.ends_with?(&1, "web"))
-    |> hd
-    |> String.split("_")
-    |> Enum.drop(-1)
-    |> Enum.map(&String.capitalize(&1))
-    |> Enum.join
-  end
-
 end
